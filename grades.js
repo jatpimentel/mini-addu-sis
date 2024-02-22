bff43fe864240d37179edfd8cb81014bb03d2aa4
jatpimentel <jobaaron444@gmail.com>
2024-02-22 16:35:49 +0800

    COURSES page

--- grades.js
+++ grades.js
@@ -1,4 +1,4 @@
-const students = [
+const student_grades = [
     {
 
         Column1: "ASF 4204",
@@ -41,12 +41,54 @@ for (let student of students){
 }
 */
 
-for (let student of students){
+for (let grades of student_grades){
     const newRow = studentData.insertRow(0)
 
-    for(let field in student){
+    for(let field in grades){
         const newCell = newRow.insertCell()
 
-        newCell.innerHTML = student[field]
+        newCell.innerHTML = grades[field]
+    }
+}
+
+/*------------------ COURSES DATA ---------------- */
+
+
+const courses = [
+    {
+
+        Column1: "ASF 4204",
+        Column2: "SENIOR'S INTEGRATION PROGRAM",
+        Column3: "A",
+        Column4: "B",
+        Column5: "B",
+        Column6: "A",
+        Column7: "3.00",
+        Column8: "",
+        
+    },
+    {
+        Column1: "SUBJ NO",
+        Column2: "DESCRIPTIVE TITLE",
+        Column3: "PRELIM",
+        Column4: "MIDTERM",
+        Column5: "PRE-FINAL",
+        Column6: "FINAL",
+        Column7: "UNITS",
+        Column8: "CREDIT TO"
+
+    }
+]
+
+
+const courseData = document.getElementById('course-data').getElementsByTagName('tbody')[0]
+
+for (let course of courses){
+    const newRow = courseData.insertRow(0)
+
+    for(let field in course){
+        const newCell = newRow.insertCell()
+
+        newCell.innerHTML = course[field]
     }
 }
